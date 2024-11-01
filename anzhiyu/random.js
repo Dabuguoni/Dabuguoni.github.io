991604c9d6fd08887a709382b44ab18c36358876
Dabuguoni <1684012385@qq.com>
2024-11-01 12:23:24 +0800

    Site updated: 2024-11-01 12:23:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/29/JavaNote/","2024/10/29/Shentou/"];function toRandomPost(){
+var posts=["2024/10/29/Shentou/","2024/10/29/JavaNote/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
